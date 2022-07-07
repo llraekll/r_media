901557d9e73c907b76426bbcad1024616f1dbacf
llraekll <rakshith1@gmail.com>
2022-07-07 15:19:34 +0530

    comit

--- base/models.py
+++ base/models.py
@@ -1,2 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profileimg = models.ImageField(
+        upload_to='profile_image', default='default_profile_image.jpg')
+    location = models.CharField(max_length=100, blank=True)
+
+
+def __str__(self):
+    return self.user.username
