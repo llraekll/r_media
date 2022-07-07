3331157210be46e753146c9510fd6f8312bdb4cc
llraekll <rakshith1@gmail.com>
2022-07-07 18:01:19 +0530

    comitt

--- base/urls.py
+++ base/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     # the name index here is the name of the fun defined in views file. Keep the name in ' '.
     path('', views.index, name='index'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,46 @@
-from django.shortcuts import render
+from email import message
+from urllib import request
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import Profile
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def signup(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already exists')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username unavailable')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                user_model= User.objects.get(username=username)
+                new_profile= Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
+        else:
+            messages.info(request, 'Passwords do not match')
+            return redirect('signup')
+
+    else:
+        return render(request, 'signup.html')
+
+
+def signin(request):
+    return render(request, 'signin.html')
