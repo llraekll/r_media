e4a7a07f984e73cba11b099390a2d1e4051b23a4
llraekll <rakshith1@gmail.com>
2022-07-08 16:24:19 +0530

    sjd

--- base/views.py
+++ base/views.py
@@ -27,11 +27,13 @@ def signup(request):
                 messages.info(request, 'Username unavailable')
                 return redirect('signup')
             else:
-                user = User.objects.create_user(username=username, email=email, password=password)
+                user = User.objects.create_user(
+                    username=username, email=email, password=password)
                 user.save()
 
-                user_model= User.objects.get(username=username)
-                new_profile= Profile.objects.create(user=user_model, id_user=user_model.id)
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(
+                    user=user_model, id_user=user_model.id)
                 new_profile.save()
                 return redirect('signup')
         else:
@@ -43,4 +45,23 @@ def signup(request):
 
 
 def signin(request):
-    return render(request, 'signin.html')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Invalid credentials')
+            return redirect('signin')
+
+    else:
+        return render(request, 'signin.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
