2af748b9f46e15b997b099017eda34dadb94f799
llraekll <rakshith1@gmail.com>
2022-07-12 01:04:07 +0530

    salkdj

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Profile, Post
+from .models import Profile, Post, LikePost
 
 admin.site.register(Profile)
 admin.site.register(Post)
+admin.site.register(LikePost)
 # Register your models here.
--- base/urls.py
+++ base/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('settings', views.settings, name='settings'),
     path('upload', views.upload, name='upload'),
+    path('like-post', views.like_post, name='like-post'),
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
     path('logout', views.logout, name='logout'),
