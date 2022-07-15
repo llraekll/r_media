4c0279c302befb327e4fe6ec447499939dfb586f
llraekll <rakshith1@gmail.com>
2022-07-15 14:23:14 +0530

    commit

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from email import message
 from urllib import request
+from webbrowser import get
 import django
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
@@ -213,3 +214,25 @@ def follow(request):
             return redirect('/profile/'+user)
     else:
         return redirect('/')
+
+@login_required(login_url='signin')
+def search(request):
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        username_object = User.objects.filter(username__icontains=username)
+
+        username_profile = []
+        username_profile_list = []
+
+        for users in username_object:
+            username_profile.append(users.id)
+
+        for ids in username_profile:
+            profile_lists = Profile.objects.filter(id_user=ids)
+            username_profile_list.append(profile_lists)
+        
+        username_profile_list = list(chain(*username_profile_list))
+    return render(request, 'search.html', {'user_profile': user_profile, 'username_profile_list': username_profile_list})
