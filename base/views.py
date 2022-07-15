57648de01f702df5d86cc23146c5746132762740
llraekll <rakshith1@gmail.com>
2022-07-15 15:13:13 +0530

    commit

--- base/views.py
+++ base/views.py
@@ -38,7 +38,7 @@ def index(request):
         user_list = User.objects.get(username=user.user)
         user_following_all.append(user_list)
 
-    new_suggestion_list = [x for x in list(all_users) if (x not in list(user_following_all()))]
+    new_suggestion_list = [x for x in list(all_users) if (x not in list(user_following_all))]
     current_user = User.objects.filter(username=request.user.username)
     final_suggestion_list =[ x for x in list(new_suggestion_list) if ( x not in list(current_user))]
     random.shuffle(final_suggestion_list)
@@ -49,9 +49,9 @@ def index(request):
     for users in final_suggestion_list:
         username_profile.append(users.id)
 
-    for ids in user_profile:
-        profile_list = Profile.objects.filter(id_user=ids)
-        username_profile_list.append(profile_list)
+    for ids in username_profile:
+        profile_lists = Profile.objects.filter(id_user=ids)
+        username_profile_list.append(profile_lists)
 
     suggestions_username_profile_list = list(chain(*username_profile_list))
 
